1ee380f20059b8f789849ae9d91b142757bffb9e
Annie <anniecmartens@gmail.com>
2019-01-12 13:46:17 -0600

    token in .env

--- corgi.rb
+++ corgi.rb
@@ -17,7 +17,7 @@ post '/' do
 		convo_id = response["data"]["item"]["id"]
 		@intercom.conversations.reply(:id => convo_id, :type => 'admin', :admin_id => '1283891',
 								  :message_type => 'comment', 
-								  :body => '<html><body><img src="https://i.giphy.com/media/KLbq09EoE1uDu/giphy.webp"></body></html>')
+								  :body => '<html><body>corgo<img src="https://i.giphy.com/media/KLbq09EoE1uDu/giphy.webp"></body></html>')
 
 	end
 end
@@ -25,7 +25,7 @@ end
 def initialize_intercom
 	if @intercom.nil? then
 		token = ENV['TOKEN']
-		@intercom = Intercom::Client.new(token: 'dG9rOmI3NTFjMjE1X2NhMThfNGEwOF9iNGEwXzgzZDMzMTQ3YTU0YzoxOjA=')
+		@intercom = Intercom::Client.new(token: token)
 	end
 end
 
