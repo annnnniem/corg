80d493846f238c51ad1edbb677747fa51d122014
Annie <anniecmartens@gmail.com>
2019-01-12 13:34:17 -0600

    first draft

--- corgi.rb
+++ corgi.rb
@@ -3,33 +3,30 @@ require 'dotenv/load'
 require 'intercom'
 require 'json'
 
+get '/' do 
+	"hi"
+end
+
+post '/' do 
+	response = JSON.parse(request.body.read)
+	conversation_text = response["data"]["item"]["conversation_message"]["body"]
+	puts conversation_text
+	if conversation_text.include? "corgi"
+		puts "Convo contains corgi"
+		initialize_intercom
+		convo_id = response["data"]["item"]["id"]
+		@intercom.conversations.reply(:id => convo_id, :type => 'admin', :admin_id => '1283891',
+								  :message_type => 'comment', 
+								  :body => '<html><body><img src="https://i.giphy.com/media/KLbq09EoE1uDu/giphy.webp"></body></html>')
+
+	end
+end
+
 def initialize_intercom
 	if @intercom.nil? then
 		token = ENV['TOKEN']
-		@intercom = Intercom::Client.new(token: ENV['token'])
+		@intercom = Intercom::Client.new(token: 'dG9rOmI3NTFjMjE1X2NhMThfNGEwOF9iNGEwXzgzZDMzMTQ3YTU0YzoxOjA=')
 	end
 end
 
-def get_convo
-	convo_content = JSON.parse(@intercom.conversations.find(:id => convo_id))
-	puts convo_content
-end
-
-def is_corgi_convo
-	if convo_content["corgi" | "Corgi"]
-		puts "convo includes corgi"
-	end
-end
-
-def send_reply
-	@intercom.conversations.reply(:id => convo_id, :type => 'admin', 
-								  :message_type => 'comment', 
-								  :body => '<html><body><img src="https://gph.is/1cpsZE7"></body></html>')
-end
-
-post '/' do
-	convo_id = JSON.parse(request.body.read)["data"]["item"]["id"]
-	puts convo_id
-	get_convo(convo_id)
-end
-
+#ask joel: moving this into methods -- how do I set up my variables?
